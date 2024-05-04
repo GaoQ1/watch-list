55e5dd3ee33ac8cc8ddb5c6aa09a8964b2fd6768
权高 <quangao@quangaodebijibendiannao.local>
2024-05-04 16:10:29 +0800

    edit redirectUrl

--- app/components/AuthForm.jsx
+++ app/components/AuthForm.jsx
@@ -11,7 +11,7 @@ export default function AuthForm(){
         view="magic_link"
         showLinks={false}
         providers={[]}
-        redirectTo='/auth/callback'
+        redirectTo='https://watch-list-plum.vercel.app/auth/callback'
         appearance={{
             theme: 'dark',
             button: {
